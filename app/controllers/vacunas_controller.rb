37bbda7483c554b9e624529350d3874551970e7c
Clara Barelli <cbarelli@gba.gob.ar>
2021-10-31 19:11:15 -0300

    Vista del historial de vacunas

--- app/controllers/vacunas_controller.rb
+++ app/controllers/vacunas_controller.rb
@@ -1,6 +1,6 @@
 class VacunasController < ApplicationController
     before_action :authenticate_user!
     def index
-        @vacunas=Vacuna.where(user: current_user)
+        @turnos=Turno.where(user: current_user)
     end
 end
--- app/models/turno.rb
+++ app/models/turno.rb
@@ -1,4 +1,4 @@
 class Turno < ApplicationRecord
-    enum estado: [:pendiente, :cancelado, :completado]
+    enum estado: [:pendiente, :cancelado, :completado] #Podriamos poner un enum que sea previoRegistro o algo asi
     belongs_to :user
 end
