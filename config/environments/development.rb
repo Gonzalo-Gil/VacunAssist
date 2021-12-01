0e7c8a6c47447524d78d9b65e3578836b97a1b0c
Gonza-Gil <gonzalogil123@gmail.com>
2021-12-01 13:28:56 -0300

    Config para poder mandar mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000' #replace with your own url
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "vacunassist04@gmail.com",
+    :password             => "grupo04is",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
