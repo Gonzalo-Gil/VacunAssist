4fe3ea41be05d72c1de511f404983cb247494864
Gonza-Gil <gonzalogil123@gmail.com>
2021-12-03 10:55:13 -0300

    cambio al migrate estado turno

--- db/migrate/20211030200818_agregar_estado_al_turno.rb
+++ db/migrate/20211030200818_agregar_estado_al_turno.rb
@@ -1,5 +1,5 @@
 class AgregarEstadoAlTurno < ActiveRecord::Migration[6.1]
   def change
-    add_column :turnos, :estado, :integer, default 0
+    add_column :turnos, :estado, :integer, default: 0
   end
 end
